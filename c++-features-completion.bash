edfa48c679c01ea79ccd942dd28f2aa77816d689
Ulrich Drepper <drepper@gmail.com>
2022-01-17 16:25:37 +0100

    Add bash completion script

---
+++ c++-features-completion.bash
@@ -0,0 +1,16 @@
+#!/bin/bash
+_cpp_features_completion()
+{
+  if [ ${#COMP_WORDS[@]} -gt 3 ]; then
+    return
+  fi
+  all=$(g++ --help -v|& sed -n 's/^  -std=\(\(c\|gnu\)++[^ ]*\).*/\1/p')
+  if [ ${#COMP_WORDS[@]} -eq 1 ]; then
+    COMPREPLY=($all)
+  elif [[ " ${all[*]} " =~ " ${COMP_WORDS[1]} " ]]; then
+    COMPREPLY=($all)
+  else
+    COMPREPLY=($(compgen -W "$all" -- "${COMP_WORDS[$((${#COMP_WORDS[@]} - 1))]}"))
+  fi
+}
+complete -F _cpp_features_completion c++-features.sh
